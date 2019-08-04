a37f6f0d3d4ee3ccf1b429ced19af279193205cd
Vincent Ambo <tazjin@google.com>
2019-08-04 22:45:23 +0100

    feat(build): Build Nixery book and embed it into Nixery image
    Executes the previously added mdBook on the previously added book
    source to yield a directory that can be served by Nixery on its index
    page.
    
    This is one of those 'I <3 Nix' things due to how easy it is to do.

---
+++ docs/default.nix
@@ -0,0 +1,36 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Builds the documentation page using the Rust project's 'mdBook'
+# tool.
+#
+# Some of the documentation is pulled in and included from other
+# sources.
+
+{ fetchFromGitHub, mdbook, runCommand }:
+
+let
+  nix-1p = fetchFromGitHub {
+    owner  = "tazjin";
+    repo   = "nix-1p";
+    rev    = "aab846cd3d79fcd092b1bfea1346c587b2a56095";
+    sha256 = "12dl0xrwgg2d4wyv9zxgdn0hzqnanczjg23vqn3356rywxlzzwak";
+  };
+in runCommand "nixery-book" {} ''
+  mkdir -p $out
+  cp -r ${./.}/* .
+  chmod -R a+w src
+  cp ${nix-1p}/README.md src/nix-1p.md
+  ${mdbook}/bin/mdbook build -d $out
+''
