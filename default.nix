cca0c9813807d2e4843158dbb71b8659034daf92
Vincent Ambo <tazjin@google.com>
2019-08-12 15:14:24 +0100

    refactor: Move registry server to a subfolder

--- default.nix
+++ default.nix
@@ -23,29 +23,11 @@ rec {
   # Users will usually not want to use this directly, instead see the
   # 'nixery' derivation below, which automatically includes runtime
   # data dependencies.
-  nixery-server = buildGoPackage {
-    name = "nixery-server";
-
-    # Technically people should not be building Nixery through 'go get'
-    # or similar (as other required files will not be included), but
-    # buildGoPackage requires a package path.
-    goPackagePath = "github.com/google/nixery";
-    goDeps = ./go-deps.nix;
-    src    = ./.;
-
-    meta = {
-      description = "Container image build serving Nix-backed images";
-      homepage    = "https://github.com/google/nixery";
-      license     = lib.licenses.asl20;
-      maintainers = [ lib.maintainers.tazjin ];
-    };
-  };
+  nixery-server = callPackage ./server {};
 
   # Nix expression (unimported!) which is used by Nixery to build
   # container images.
-  nixery-builder = runCommand "build-registry-image.nix" {} ''
-    cat ${./build-registry-image.nix} > $out
-  '';
+  nixery-builder = ./build-registry-image.nix;
 
   # nixpkgs currently has an old version of mdBook. A new version is
   # built here, but eventually the update will be upstreamed
