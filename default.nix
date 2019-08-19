e10a742201a5c2731bc42063bc07379c29ab4a3f
Vincent Ambo <tazjin@google.com>
2019-08-19 01:10:21 +0100

    feat(build): Add 'extraPackages' parameter
    This makes it possible to inject additional programs (e.g. Cachix)
    into a Nixery container.

--- default.nix
+++ default.nix
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-{ pkgs ? import <nixpkgs> { }, preLaunch ? "" }:
+{ pkgs ? import <nixpkgs> { }
+, preLaunch ? ""
+, extraPackages ? [] }:
 
 with pkgs;
 
@@ -93,6 +95,6 @@ rec {
       nixery-launch-script
       openssh
       zlib
-    ];
+    ] ++ extraPackages;
   };
 }
