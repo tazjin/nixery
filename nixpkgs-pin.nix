9ae1920eb2a8968c3c429dd770f53879911799ec
heroku-miraheze <61781343+heroku-miraheze@users.noreply.github.com>
2021-08-20 14:57:16 +0200

    Fix the problem.

--- nixpkgs-pin.nix
+++ nixpkgs-pin.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-21.05.zip";
-  sha256 = "cc1a2c2bb49c61c776ac774901bf8ee3c21af17fdd40c2eae51320b57b110d75";
+  sha256 = "a49c17cd71df880d82b3815b20502ca05b0a4bff42924d4ef9bd23cb8157f5d9";
 }) {}
