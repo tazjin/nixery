fda20283d968ff900ba55ddd3d7460dc68ca5710
Vincent Ambo <mail@tazj.in>
2021-10-29 17:36:05 +0200

    chore: Bump nixpkgs pin to nixos-unstable 2021-10-29

--- nixpkgs-pin.nix
+++ nixpkgs-pin.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/4263ba5e133cc3fc699c1152ab5ee46ef668e675.tar.gz";
-  sha256 = "1nzqrdw0lhbldbs9r651zmgqpwhjhh9sssykhcl2155kgsfsrk7i";
+  url = "https://github.com/NixOS/nixpkgs/archive/2deb07f3ac4eeb5de1c12c4ba2911a2eb1f6ed61.tar.gz";
+  sha256 = "0036sv1sc4ddf8mv8f8j9ifqzl3fhvsbri4z1kppn0f1zk6jv9yi";
 }) {}
